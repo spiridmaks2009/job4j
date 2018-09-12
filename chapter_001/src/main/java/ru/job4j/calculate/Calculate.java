e96bfc274328c5a4cda39703697678fb2f1d63ca
spiridmaks2009 <spiridmaks@gmail.com>
2018-09-13 00:01:29 +0300

    1.3 JavaDoc, Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+
+/**
+* Calculate
+*
+* @author Maxim Spiridonov (spiridmaks@gmail.com)
+*/
 public class Calculate {
+	/**
+    * Main.
+    *
+    * @param args - args
+    */
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 	}
