80913b9c930a2d5ebb7db4992c465fe4dd0c860e
spiridmaks2009 <spiridmaks@gmail.com>
2018-09-27 23:32:13 +0300

    Task 5.1.1. MatrixIterator

--- junior/src/main/java/ru/job4j/iterator/ArrayIterator.java
+++ junior/src/main/java/ru/job4j/iterator/ArrayIterator.java
@@ -3,17 +3,35 @@ package ru.job4j.iterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * ArrayIterator
+ *
+ * @author maksimspiridonov
+ */
 public class ArrayIterator implements Iterator {
 
+    /**
+     * Содержит исходный массив
+     */
     private int[][] outerArray;
-    private int[] innerArray;
 
-    private int outerIndex = 0, innerIndex = 0;
+    /**
+     * Содержит индекс внешнего массива
+     */
+    private int outerIndex;
 
+    /**
+     * Содержит индекс внутреннего массива
+     */
+    private int innerIndex;
+
+    /**
+     * Конструктор
+     *
+     * @param outerArray
+     */
     public ArrayIterator(int[][] outerArray) {
-        if(outerIndex >= outerArray.length) throw new NoSuchElementException();
         this.outerArray = outerArray;
-        this.innerArray = outerArray[outerIndex++];
     }
 
     /**
@@ -23,13 +41,11 @@ public class ArrayIterator implements Iterator {
     @Override
     public boolean hasNext() {
 
-        if (innerArray.length <= innerIndex) {
-            if (outerArray.length > outerIndex) {
-                innerArray = outerArray[outerIndex++];
-                innerIndex = 0;
-            }
+        boolean flag = true;
+        if (!(outerIndex < outerArray.length && innerIndex < outerArray[outerIndex].length)) {
+            flag = false;
         }
-        return innerArray.length > innerIndex;
+        return flag;
     }
 
     /**
@@ -37,15 +53,20 @@ public class ArrayIterator implements Iterator {
      * @throws NoSuchElementException if the iteration has no more elements
      */
     @Override
-    public Object next() throws NoSuchElementException{
-
+    public Object next() throws NoSuchElementException {
         if (!hasNext()) {
-            if (outerArray.length > outerIndex) {
-                innerArray = outerArray[outerIndex++];
-                innerIndex = 0;
-            }
+            throw new NoSuchElementException();
         }
-        return innerArray[innerIndex++];
+        int[] innerArray = outerArray[outerIndex];
+        int number = 0;
+        if (innerIndex < innerArray.length - 1) {
+            number  = innerArray[innerIndex++];
+        } else if (innerIndex == innerArray.length - 1) {
+            number = innerArray[innerIndex];
+            outerIndex++;
+            innerIndex = 0;
+        }
+        return number;
     }
 
 }
