e0c65a74f4edaf9c9690c727d5e6b93dee645ec8
spiridmaks2009 <spiridmaks@gmail.com>
2018-09-28 21:22:59 +0300

    Task 5.1.4. IteratorOfIterators

--- junior/src/main/java/ru/job4j/iterator/Converter.java
+++ junior/src/main/java/ru/job4j/iterator/Converter.java
@@ -3,12 +3,28 @@ package ru.job4j.iterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Converter implements Iterator{
+/**
+ * Converter class
+ *
+ * @author maksimspiridonov
+ */
+public class Converter implements Iterator {
 
+    /**
+     * Итератор итераторов
+     */
     private Iterator<Iterator<Integer>> iterator;
+
+    /**
+     * Внутренний итератор
+     */
     private Iterator<Integer> innerIterator;
 
 
+    /**
+     * @param it
+     * @return
+     */
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
         this.iterator = it;
         this.innerIterator = it.next();
@@ -20,8 +36,11 @@ public class Converter implements Iterator{
      */
     @Override
     public boolean hasNext() {
-        if (innerIterator.hasNext()) return true;
-        else if (iterator.hasNext()) return true;
+        if (innerIterator.hasNext()) {
+            return true;
+        } else if (iterator.hasNext()) {
+            return true;
+        }
         return false;
     }
 
@@ -30,9 +49,10 @@ public class Converter implements Iterator{
      * @throws NoSuchElementException if the iteration has no more elements
      */
     @Override
-    public Object next() {
-        if(!innerIterator.hasNext())
+    public Integer next() {
+        if (!innerIterator.hasNext()) {
             innerIterator = iterator.next();
+        }
         return innerIterator.next();
     }
 }
