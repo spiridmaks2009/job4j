6cf8f0074715c07a3131f05435a8100eef31cf7f
spiridmaks2009 <spiridmaks@gmail.com>
2018-10-14 22:29:23 +0300

    Map task 1.

---
+++ junior/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,19 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author maksimspiridonov
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
