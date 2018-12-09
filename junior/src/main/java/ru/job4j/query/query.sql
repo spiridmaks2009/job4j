456bae6b9d286908ff6e64549fd967340d2517bc
spiridmaks2009 <spiridmaks@gmail.com>
2018-12-09 14:16:49 +0300

    2. Query

---
+++ junior/src/main/java/ru/job4j/query/query.sql
@@ -0,0 +1,32 @@
+1) select * from product as p, type as t
+   where t.name = 'СЫР' and t.id = p.type_id;
+
+2) select * from product
+   where name like '%мороженое%';
+
+3) select * from products
+   where month(expired_date) - month(now()) = 1;
+
+4) select * from product as p
+   where p.price = (select max(price) from product);
+
+5) select t_p.type_name, count(t_p.product_id) from
+        (select p.id as product_id, p.name as product_name,
+                t.id as type_id, t.name as type_name from product as p
+         right join type as t on t.id = p.type_id) as t_p
+   group by t_p.type_id;
+
+6) select * from product as p, type as t
+      where t.name = 'СЫР' or where t.name = 'МОЛОКО' and t.id = p.type_id;
+
+ 7) select * from t2
+        (select t_p.type_name, count(t_p.product_id) as count_product from
+               (select p.id as product_id, p.name as product_name,
+                       t.id as type_id, t.name as type_name from product as p
+                right join type as t on t.id = p.type_id) as t_p
+         group by t_p.type_id) as t2
+    where count_product < 10;
+
+ 8) select p.name, t.name from product
+    left join type as t
+    on p.type_id = t.id;
